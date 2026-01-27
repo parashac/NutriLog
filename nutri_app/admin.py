1b84203a9d0639296be73d2e8ce50382eeeb43e9
parashac <parashacharya139@gmail.com>
2026-01-27 17:12:29 +0545

    models

--- nutri_app/admin.py
+++ nutri_app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-
+from nutri_app.models import Fit
 # Register your models here.
-admin.site.register(admin)
+admin.site.register(Fit)
--- nutri_app/models.py
+++ nutri_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Fit:
+    age = models.IntegerField(max_length=2)
+    height = models.IntegerField(max_length=10)
+    weight = models.IntegerField(max_length=10)
+
+
+    def __str__(self):
+        return self.age
