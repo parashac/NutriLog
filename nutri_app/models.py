136ce80ff3a73ddac22488ab1244ea0baddeb8d4
parashac <parashacharya139@gmail.com>
2026-01-27 17:35:38 +0545

    models.py for fit

--- nutri_app/models.py
+++ nutri_app/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Fit:
+class Fit(models.Model):
+    user = models.OneToOneField
     age = models.IntegerField(max_length=2)
+    gender = models.CharField(max_length=10)
     height = models.IntegerField(max_length=10)
     weight = models.IntegerField(max_length=10)
+    food = models.CharField(max_length=100)
+    meal =  models.CharField(max_length=100)
+    created_at = models.TimeField(auto_now=True)
+    updated_at = models.TimeField(auto_now=True)
 
-
-    def __str__(self):
+    def __init__(self):
         return self.age
